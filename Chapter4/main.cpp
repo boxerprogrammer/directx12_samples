2f876b93373af6632a963b95811c2bf88cf60979
川野竜一 <r_kawano@asojuku.ac.jp>
2019-04-08 21:02:22 +0900

    アダプタ＆デバイス生成

--- Chapter4/main.cpp
+++ Chapter4/main.cpp
@@ -1,8 +1,17 @@
 #include<Windows.h>
 #include<tchar.h>
+#include<d3d12.h>
+#include<dxgi1_6.h>
+#include<vector>
 #ifdef _DEBUG
 #include<iostream>
 #endif
+
+
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+
 using namespace std;
 
 ///@brief コンソール画面にフォーマット付き文字列を表示
@@ -29,6 +38,9 @@ LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 
 const unsigned int window_width=1280;
 const unsigned int window_height=720;
+
+IDXGIFactory6* _dxgiFactory = nullptr;
+ID3D12Device* _dev = nullptr;
 
 
 #ifdef _DEBUG
@@ -61,6 +73,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		nullptr,//メニューハンドル
 		w.hInstance,//呼び出しアプリケーションハンドル
 		nullptr);//追加パラメータ
+
+	D3D_FEATURE_LEVEL levels[] = {
+	D3D_FEATURE_LEVEL_12_1,
+	D3D_FEATURE_LEVEL_12_0,
+	D3D_FEATURE_LEVEL_11_1,
+	D3D_FEATURE_LEVEL_11_0,
+	};
+	auto result = CreateDXGIFactory1(IID_PPV_ARGS(&_dxgiFactory));
+	std::vector <IDXGIAdapter*> adapters;
+	IDXGIAdapter* tmpAdapter = nullptr;
+	for (int i = 0; _dxgiFactory->EnumAdapters(i, &tmpAdapter) != DXGI_ERROR_NOT_FOUND; ++i) {
+		adapters.push_back(tmpAdapter);
+	}
+	for (auto adpt : adapters) {
+		DXGI_ADAPTER_DESC adesc = {};
+		adpt->GetDesc(&adesc);
+		std::wstring strDesc = adesc.Description;
+		if (strDesc.find(L"NVIDIA") != std::string::npos) {
+			tmpAdapter = adpt;
+			break;
+		}
+	}
+
+	//Direct3Dデバイスの初期化
+	D3D_FEATURE_LEVEL featureLevel;
+	for (auto l : levels) {
+		if (D3D12CreateDevice(tmpAdapter, l, IID_PPV_ARGS(&_dev)) == S_OK) {
+			featureLevel = l;
+			break;
+		}
+	}
+
 
 	ShowWindow(hwnd, SW_SHOW);//ウィンドウ表示
 	MSG msg = {};
