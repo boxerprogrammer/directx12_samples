a9b12dc27fc80dd95f44f646fee22bf3bcc679be
boxer_programmer <zan56011@nifty.com>
2019-05-16 02:27:10 +0900

    シェーダ追加

--- Chapter5/BasicShader.hlsl
+++ Chapter5/BasicShader.hlsl
@@ -1,7 +1,7 @@
-float4 BasicVS(float4 pos:POSITION) :SV_POSITION{
+float4 BasicVS(float4 pos : POSITION) :SV_POSITION{
 	return pos;
 }
 
 float4 BasicPS(float4 pos : SV_POSITION) : SV_TARGET{
-	return float(1,1,1,1);
+	return float4(1,1,1,1);
 }
