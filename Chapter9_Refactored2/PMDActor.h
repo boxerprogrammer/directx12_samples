d0b37c0116edffad1fc9ccbe577dbebe1ab397fc
Ryuichi Kawano <r_kawano@asojuku.ac.jp>
2019-08-30 19:36:37 +0900

    つらいー

--- Chapter9_Refactored2/PMDActor.h
+++ Chapter9_Refactored2/PMDActor.h
@@ -9,7 +9,13 @@ class PMDActor
 private:
 	template<typename T>
 	using ComPtr = Microsoft::WRL::ComPtr<T>;
+	ComPtr<ID3D12Resource> _vb=nullptr;
+	ComPtr<ID3D12Resource> _ib=nullptr;
+	D3D12_VERTEX_BUFFER_VIEW _vbv = {};
+	D3D12_INDEX_BUFFER_VIEW _ibv = {};
 	ComPtr< ID3D12DescriptorHeap> _materialHeap = nullptr;//マテリアルヒープ(5個ぶん)
+	bool LoadPMD(const char* filepath);
+
 public:
 	PMDActor(const char* filepath);
 	~PMDActor();
