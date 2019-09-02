e2c7e1900f8d13f48bcd2d8c8163a8d8a4449366
r_kawano <r_kawano@asojuku.ac.jp>
2019-09-02 11:18:30 +0900

    コミットコミット

--- Chapter9_Refactored2/Dx12Wrapper.h
+++ Chapter9_Refactored2/Dx12Wrapper.h
@@ -1,6 +1,14 @@
 #pragma once
+#include<d3d12.h>
+#include<map>
+#include<d3dcompiler.h>
+#include<wrl.h>
+#include<string>
 class Dx12Wrapper
 {
+	template<typename T>
+	using ComPtr = Microsoft::WRL::ComPtr<T>;
+	std::map<std::string,ComPtr<ID3D12Resource>> _textureTable;
 public:
 	Dx12Wrapper();
 	~Dx12Wrapper();
--- Chapter9_Refactored2/PMDActor.cpp
+++ Chapter9_Refactored2/PMDActor.cpp
@@ -9,3 +9,9 @@ PMDActor::PMDActor(const char* filepath)
 PMDActor::~PMDActor()
 {
 }
+
+
+bool 
+PMDActor::LoadPMD(const char* filepath) {
+	return false;
+}
