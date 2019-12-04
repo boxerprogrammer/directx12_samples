6b37fdd4f605819bb8c9b5c3ae4260942a331660
Ryuichi Kawano <r_kawano@asojuku.ac.jp>
2019-12-04 20:31:18 +0900

    法線ベクトルを回転に反映するよう修正

--- Chapter11/BasicShader.hlsl
+++ Chapter11/BasicShader.hlsl
@@ -45,7 +45,7 @@ Output BasicVS(float4 pos : POSITION , float4 normal : NORMAL, float2 uv : TEXCO
 	output.svpos = mul(mul(proj,view),pos);//シェーダでは列優先なので注意
 	output.pos = mul(view, pos);
 	normal.w = 0;//ここ重要(平行移動成分を無効にする)
-	output.normal = mul(world,normal);//法線にもワールド変換を行う
+	output.normal = mul(world,mul(bm,normal));//法線にもボーンとワールド変換を行う
 	output.vnormal = mul(view, output.normal);
 	output.uv = uv;
 	output.ray = normalize(output.pos.xyz - mul(view,eye));//視線ベクトル
